8e44f32892000cb5c6a4d78b702ab2d7835e5594
Sinbad Iqbal <ask2iqbal@gmail.com>
2014-11-17 12:13:35 +0530

    Remove Secrets and Constant

--- AWSImageUploadDwnldPOC/src/com/example/awsimageuploadpoc/Constants.java
+++ AWSImageUploadDwnldPOC/src/com/example/awsimageuploadpoc/Constants.java
@@ -1,32 +1,10 @@
-/*
- * Copyright 2010-2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 package com.example.awsimageuploadpoc;
 
 import java.util.Locale;
 
 public class Constants {
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    // This sample App is for demonstration purposes only.
-    // It is not secure to embed your credentials into source code.
-    // Please read the following article for getting credentials
-    // to devices securely.
-    // http://aws.amazon.com/articles/Mobile/4611615499399490
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	
-	public static final String ACCESS_KEY_ID = "AKIAJ75PY4FZPJ2UWJNQ";
-	public static final String SECRET_KEY = "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";
+	public static final String ACCESS_KEY_ID = "";
+	public static final String SECRET_KEY = "";
 	
 	public static final String PICTURE_BUCKET = "eventimagebucket";
 	public static final String PICTURE_NAME = "iqbal";
--- AWSImageUploadDwnldPOC/src/com/example/awsimageuploadpoc/MyApp.java
+++ AWSImageUploadDwnldPOC/src/com/example/awsimageuploadpoc/MyApp.java
@@ -6,7 +6,7 @@ import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
 
-@ReportsCrashes(formKey = " ", mailTo = "ask2iqbal@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.action_settings)
+@ReportsCrashes(formKey = " ", mailTo = "", mode = ReportingInteractionMode.TOAST, resToastText = R.string.action_settings)
 public class MyApp extends Application {
 
 	@Override
